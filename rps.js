18cb66a5d05624cd1027b5a0c0c2a81420455fb8
Alexander Einshøj Lund <alexander.lund1@gmail.com>
2019-04-19 18:00:32 +0200

    Small changes made

--- rps.js
+++ rps.js
@@ -1,3 +1,5 @@
+
+
 // Function that makes the computer play either rock paper or scissors
 function computerPlay(){
     let randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -34,6 +36,38 @@ function playRound(user, computer) {
         return roundOutcome;
 }
 
+
 let userinput = prompt('Choose rock, paper or scissors!').toLowerCase();
 
 alert(playRound(userinput, computerPlay()))
+console.log(game())
+
+
+
+
+
+
+
+/*
+// function to play one round of the game
+function playRound(user, computer) {
+    let roundOutcome;
+    // Makes the player choose either rock paper or scissors using a prompt
+    let userinput = prompt('Choose rock, paper or scissors!').toLowerCase();
+        if (user === 'rock' && computer === 'scissors'){
+            roundOutcome = 'Rock beats Scissors! you win!';
+        }else if (user === computer){
+            roundOutcome = 'It´s a tie!';
+        }else if (user === 'rock' && computer === 'paper'){
+            roundOutcome = 'Paper beats rock! Computer wins!';
+        }else if (user === 'paper' && computer === 'rock'){
+            roundOutcome = 'Paper beats rock! You win!';
+        }else if (user === 'paper' && computer === 'scissors'){
+            roundOutcome = 'Scissors beat Paper! Computer wins!';
+        }else if (user === 'scissors' && computer === 'rock'){
+            roundOutcome = 'Rock beats Scissors! Computer wins!';
+        }else if (user === 'scissors' && computer === 'paper'){
+            roundOutcome = 'Scissors beat paper! You win!';
+        }
+        return roundOutcome;
+} */
